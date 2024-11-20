86838258e4922266a66e32b303ffd7cada120f85
Gabriele-Viola <gabrieleviola50@gmail.com>
2024-11-20 15:51:02 +0100

    add handleformsubmit

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,16 @@ import './App.css'
 const api_server = 'http://localhost:3000'
 const api_endpoint = '/ricette'
 
+const initialFormData = {
+  title: '',
+  content: ''
+}
 
 function App() {
 
   const [ricette, setRicette] = useState({})
-
+  const [formData, setFormDeta] = useState()
+  // const [newRicetta, setNewRicetta] = ({})
 
   function fetchData(url = `${api_server}${api_endpoint}`) {
     fetch(url)
@@ -17,18 +22,38 @@ function App() {
         console.log(data);
         setRicette(data)
       })
+  }
+  function handleFormSubmit(e) {
+    e.preventDefault()
+    console.log('form state');
+
+  }
+  function handleFormfil(e) {
 
   }
   useEffect(fetchData, [])
 
+  // function handleForm(e) {
+
+  // }
 
   return (
     <>
       <h1>Le mie ricette</h1>
+      <section className='operationSect'>
+        <form onSubmit={handleFormSubmit}>
+          <label htmlFor="title">Nome ricetta</label>
+          <input type="text" name='title' id='title' />
+          <label htmlFor="content">Descrizione</label>
+          <input type="text" name='content' id='content' />
+          <button type='submit'>Aggiungi ricetta</button>
+        </form>
+
+      </section>
       <section>
         <div className="container">
           {ricette.data ? ricette.data.map(ricetta => (
-            <div className='card'>
+            <div key={ricetta.id} className='card'>
               <h3>{ricetta.title}</h3>
               <p>{ricetta.content}</p>
             </div>
