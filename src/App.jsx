d868a8c479d3a658afaba8dc304ecde2a7b7479f
Gabriele-Viola <gabrieleviola50@gmail.com>
2024-11-20 14:58:55 +0100

    add ajax call

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
 
+const api_server = 'http://localhost:3000'
+const api_endpoint = '/ricette'
+
+
+
 function App() {
-  const [count, setCount] = useState(0)
+
+  function fetchData(url = `${api_server}${api_endpoint}`) {
+    fetch(url)
+      .then(resp => resp.json())
+      .then(data => {
+        console.log(data);
+
+      })
+
+  }
+  useEffect(fetchData, [])
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </>
   )
 }
